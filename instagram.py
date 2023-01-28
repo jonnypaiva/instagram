bbabf320fedcfaba28e0c318f2aa10d202c4a3fe
jonnypaiva <jonny_paiva@hotmail.com.br>
2023-01-28 10:04:33 -0300

    Add files via upload

---
+++ instagram.py
@@ -0,0 +1,38 @@
+import instaloader
+import re
+
+bot = instaloader.Instaloader()
+
+conta = instaloader.Profile.from_username(bot.context, 'profsandromesquita')
+
+print("Nome de usuario:", conta.username)
+print("ID: ", conta.userid)
+print("NUmero de posts:", conta.mediacount)
+num_seg = conta.followers
+seguindo = conta.followees
+print("Numero de Seguidores:", num_seg)
+print("Contas que sigo:", seguindo)
+print("Bio: ", conta.biography)
+print("URL: ", conta.external_url)
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", conta.biography)
+print(emails)
+
+top_five = instaloader.TopSearchResults(bot.context,'uniateneu')
+
+print(top_five.get_profiles())
+
+i = 0
+for contas in top_five.get_profiles():
+    i += 1
+    print(i, ":", contas)
+
+i = 0
+for hashtag in top_five.get_hashtags():
+    i += 1
+    print(i, ":", hashtag)
+
+print(top_five.get_locations())
+
+
+if num_seg > 10000:
+    print("Habilitado")
